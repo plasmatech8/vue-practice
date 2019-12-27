bf74a9a07cb5ed925f26f8b25d17ca3a73144128
Mark Connelly <com0004@gmail.com>
2019-12-27 18:27:26 +1100

    Updated readme, added vue data to app.js

---
+++ GuestList/app.js
@@ -0,0 +1,14 @@
+new Vue({
+    // el tells vue which HTML elements to control.
+    el: '#app',
+
+    // data that is used by vue. We have deccided to transfer hard-coded values
+    // into dynamic vue data.
+    // -> To insert these values into our HTML, we use {{ data }} syntax
+    data: {
+        eventDate: 'August 14th - 16th',
+        eventTitle: 'Summer Festival!',
+        eventDesc: "It's back! This years summer festival will be in the " +
+                   "beautiful countryside featuring our best line up ever!"
+    }
+});
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ See *Vue.js 2 Academy: Learn Vue Step by Step* by Chris Dixon on SkillShare.
 ### GuestList
 
 A Simple guest list web app.
+
+How to view:
+1. Open index.html in a browser (or use an live server)
+2. If you make a change to the data dictionary in `app.js`, then the values of `{{ attribute }}` in `index.html` will change on reload.
