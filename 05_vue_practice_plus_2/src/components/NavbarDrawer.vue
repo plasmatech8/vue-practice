5d7a8600f681cb47c0bfb670b108a2e1d2d1ce31
Mark Connelly <com0004@gmail.com>
2021-07-30 09:43:31 +1000

    Moved navbar into own component

---
+++ 05_vue_practice_plus_2/src/components/NavbarDrawer.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-navigation-drawer
+    app
+    expand-on-hover
+    permanent
+    :style="{ top: $vuetify.application.top + 'px', zIndex: 4 }"
+  >
+    <v-list dense nav>
+      <router-link to="/" class="navLink">
+        <v-list-item link>
+          <v-list-item-icon><v-icon>home</v-icon></v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+      </router-link>
+
+      <router-link to="/employees" class="navLink">
+        <v-list-item link>
+          <v-list-item-icon><v-icon>worker</v-icon></v-list-item-icon>
+          <v-list-item-title> Employees </v-list-item-title>
+        </v-list-item>
+      </router-link>
+
+      <router-link to="/settings" class="navLink">
+        <v-list-item link>
+          <v-list-item-icon><v-icon>settings</v-icon></v-list-item-icon>
+          <v-list-item-title> Settings </v-list-item-title>
+        </v-list-item>
+      </router-link>
+
+      <router-link
+        class="navLink"
+        v-for="item in [1, 2, 3]"
+        :key="item"
+        to="/item"
+      >
+        <v-list-item link>
+          <v-list-item-icon><v-icon> mdi-domain </v-icon></v-list-item-icon>
+          <v-list-item-title>Link {{ item }}</v-list-item-title>
+        </v-list-item>
+      </router-link>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.navLink {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
--- 05_vue_practice_plus_2/src/layouts/GeneralLayout.vue
+++ 05_vue_practice_plus_2/src/layouts/GeneralLayout.vue
@@ -14,65 +14,20 @@
     </v-main>
 
     <!-- Sidebar -->
-    <v-navigation-drawer
-      app
-      expand-on-hover
-      permanent
-      :style="{ top: $vuetify.application.top + 'px', zIndex: 4 }"
-    >
-      <v-list dense nav>
-        <router-link to="/" class="navLink">
-          <v-list-item link>
-            <v-list-item-icon><v-icon>home</v-icon></v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-        </router-link>
-
-        <router-link to="/employees" class="navLink">
-          <v-list-item link>
-            <v-list-item-icon><v-icon>worker</v-icon></v-list-item-icon>
-            <v-list-item-title> Employees </v-list-item-title>
-          </v-list-item>
-        </router-link>
-
-        <router-link to="/settings" class="navLink">
-          <v-list-item link>
-            <v-list-item-icon><v-icon>settings</v-icon></v-list-item-icon>
-            <v-list-item-title> Settings </v-list-item-title>
-          </v-list-item>
-        </router-link>
-
-        <router-link
-          class="navLink"
-          v-for="item in [1, 2, 3]"
-          :key="item"
-          to="/item"
-        >
-          <v-list-item link>
-            <v-list-item-icon><v-icon> mdi-domain </v-icon></v-list-item-icon>
-            <v-list-item-title>Link {{ item }}</v-list-item-title>
-          </v-list-item>
-        </router-link>
-      </v-list>
-    </v-navigation-drawer>
+    <NavbarDrawer />
   </v-app>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import NavbarDrawer from "../components/NavbarDrawer.vue";
 
 export default {
   name: "Layout",
+  components: { NavbarDrawer },
   data() {
     return {};
   },
   computed: mapGetters(["user"]),
 };
 </script>
-
-<style scoped>
-.navLink {
-  text-decoration: none;
-  color: inherit;
-}
-</style>
